4fc4828afb38c2edf68a230059d09c6ca9141cb6
Andreea2194 <36506524+Andreea2194@users.noreply.github.com>
2022-10-05 08:47:40 +0300

    First Commit

---
+++ src/AppShop.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import HorizontalBar from "./components/HorizontalBar";
+import Footer from "./components/Footer.js";
+
+
+function AppShop() {
+    return (
+        <React.Fragment>
+            <Navbar />
+            <HorizontalBar />
+            <Footer />
+        </React.Fragment>
+    );
+}
+export default AppShop; 
